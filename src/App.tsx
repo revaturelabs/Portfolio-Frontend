16b0c8396f51d37aadb293633cc9b04c34552ae7
Neruan-Revature <neustein.ruan@revature.net>
2021-05-10 13:27:03 -0700

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
