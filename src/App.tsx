3253b9a222380bf78e3f550ae6e9ebeac8b13265
carlcidromero <carl.romero@revature.net>
2021-05-10 15:58:34 -0700

    skeleton of project component complete

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
+import ProjectCard from './component/ProjectCard';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          
+          <ProjectCard />
         </Switch>
       </Router>
     </div>
