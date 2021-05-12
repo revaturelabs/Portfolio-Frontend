d72b23f36296e699774f635bf6cb7d92ea47e399
carlcidromero <carl.romero@revature.net>
2021-05-12 10:19:34 -0700

    full iteration of project component

--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -14,24 +14,27 @@ function HelloWorld() {
     const markdown2 = "# TEST";
     const file_name = 'testparsemd.md';
 
+    
     const [md, setMd] = useState('');
-
+    
     useEffect(()=>{
-        import(`./${file_name}`)
-            .then(res=>{
-                fetch(res.default)
-                    .then(res=>console.log(res))
-                    // .then(res=>setMd(res))
-            })
-            .catch(err=>console.log(err))
+        var md2json = require('md-2-json');
+        console.log(md2json.parse('This is markdown content'));
+        // import(`./${file_name}`)
+        //     .then(res=>{
+        //         fetch(res.default)
+        //             .then(res=>res.text())
+        //             .then(res=>setMd(res))
+        //     })
+        //     .catch(err=>console.log(err))
     });
 
   return (
         <div>
-            <ReactMarkdown>{markdown}</ReactMarkdown>
+            {/* <ReactMarkdown>{markdown}</ReactMarkdown>
             <button onClick={()=>setMarkdown("# test")}>generate markdown</button>
             <ReactMarkdown>{markdown2}</ReactMarkdown>
-            <MarkDown>{md}</MarkDown>
+            <MarkDown>{md}</MarkDown> */}
         </div>
     )
 }
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router';
-import AccountLogin from './AccountLogin';
 import HelloWorld from './HelloWorld';
-import RevatureWorkExp from './RevatureWorkExperience';
+import ProjectCard from './ProjectCard';
+import RevatureWorkExperience from './RevatureWorkExperience';
 
 class Layout extends Component {
     render() {
         return (
             <div>
-                <HelloWorld />                
-                <Route path="/login" component={AccountLogin} />
-                <RevatureWorkExp />
+                <RevatureWorkExperience />                
+                <ProjectCard />                
             </div>
             
         );
