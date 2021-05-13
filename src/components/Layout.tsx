b5b3440c4d22fbe6244bc9bd432ce3e8824e0601
carlcidromero <carl.romero@revature.net>
2021-05-12 19:41:41 -0700

    routed components

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,19 +1,21 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
-import AccountLogin from './AccountLogin';
-import ProjectCard from './ProjectCard';
-import RevatureWorkExperience from './RevatureWorkExperience';
+import React, { Component } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import AccountLogin from "./AccountLogin";
+import ProjectCard from "./ProjectCard";
+import RevatureWorkExperience from "./RevatureWorkExperience";
 
 class Layout extends Component {
-    render() {
-        return (
-            <div>
-                <Route path="/login" component={AccountLogin} />
-                <RevatureWorkExperience />
-                <ProjectCard />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login" exact component={AccountLogin} />
+          <Route path="/projects" exact component={ProjectCard} />
+          <Route path="/revature-work-experience" exact component={RevatureWorkExperience} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default Layout;
