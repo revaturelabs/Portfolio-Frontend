b3e5024888e90a239ae121d190f2e21709392564
carlcidromero <carl.romero@revature.net>
2021-05-13 14:48:24 -0700

    doing: POST and PUT

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,24 +1,28 @@
 import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import AccountLogin from "./AccountLogin";
-import ProjectCard from "./ProjectCard";
+import Project from "./Project";
 import RevatureWorkExperience from "./RevatureWorkExperience";
-import Landing from './Landing';
-import EditEmpPortfolio from './EditEmpPortfolio';
+import Landing from "./Landing";
+import EditEmpPortfolio from "./EditEmpPortfolio";
 
 function Layout() {
-    return (
-      <BrowserRouter>
-        <Switch>
-          <Route path="/login" exact component={AccountLogin} />
-          <Route path="/projects" exact component={ProjectCard} />
-          <Route path="/revature-work-experience" exact component={RevatureWorkExperience} />
-          <Route path="/" exact component={Landing} />
-          <Route path="/main" exact component={Landing} />
-          <Route path="/portfolio" exact component={EditEmpPortfolio} />
-        </Switch>
-      </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/login" exact component={AccountLogin} />
+        <Route path="/projects" exact component={Project} />
+        <Route
+          path="/revature-work-experience"
+          exact
+          component={RevatureWorkExperience}
+        />
+        <Route path="/" exact component={Landing} />
+        <Route path="/main" exact component={Landing} />
+        <Route path="/portfolio" exact component={EditEmpPortfolio} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default Layout;
