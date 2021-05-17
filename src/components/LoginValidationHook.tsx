ef38dc736cdbcdbedb212c95c22fcc860d3dec84
Peter Moore <masterpm84@gmail.com>
2021-05-17 15:20:46 -0500

    added login validation

---
+++ src/components/LoginValidationHook.tsx
@@ -0,0 +1,36 @@
+import {useState} from 'react';
+
+const useForm = (initialValues: any, loginValidate: any) => {
+    const [inputs, setInputs] = useState(initialValues)
+    const [errors, setErrors] = useState({})
+    
+
+    const handleSubmit = (event: any) => {
+        event.preventDefault()
+        const validationErrors = loginValidate(inputs)
+        console.log(validationErrors)
+        const noErros = Object.keys(validationErrors).length === 0
+        setErrors(validationErrors)
+        if(noErros) {
+            alert("Logged In")
+            window.location.reload()
+        } else {
+            console.log("Errors, please try again", validationErrors)
+        }
+    }
+
+    const handleInputChange = (event: any) => {
+        event.persist()
+        setInputs((inputs: any) => ({...inputs, [event.target.name]: event.target.value}))
+    }
+
+    return {
+        handleSubmit,
+        handleInputChange,
+        inputs,
+        errors
+    }
+
+}
+
+export default useForm;
