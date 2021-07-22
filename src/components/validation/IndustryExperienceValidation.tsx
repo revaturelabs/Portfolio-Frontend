32e1a92a579fb7b8e3ff2a4c510caf8508a6484a
faurori <faurori@gmail.com>
2021-07-22 13:21:09 -0400

    Added industry equivalency validation

--- src/components/validation/IndustryExperienceValidation.tsx
+++ src/components/validation/IndustryExperienceValidation.tsx
@@ -1,1 +1,12 @@
-export {}
+const industryEquivalency = (skillName: string) => {
+    console.log("IN INDUSTRY EQUIVALENCY VALIDATION");
+    if(skillName){
+        return true;
+    }
+    else{
+        return false;
+    }
+    
+}
+
+export default industryEquivalency;
