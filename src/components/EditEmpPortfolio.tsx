f5849b3b4a8cc94444c5bb087399e5cc22371f25
Derek <21209173+ddinh@users.noreply.github.com>
2021-07-23 15:18:04 -0500

    Fix portfolio download URL

--- src/components/EditEmpPortfolio.tsx
+++ src/components/EditEmpPortfolio.tsx
@@ -46,7 +46,7 @@ const EditEmpPortfolio = () => {
                 <Link to="/view">
                     <button className="btn btn-primary m-1">View Portfolio</button>
                 </Link>
-                <Link to={{ pathname: url + `/portfolios/full/${cookies['portfolio'].id}` }} target="_blank" >
+                <Link to={{ pathname: `${portfolioUrl}/full/${cookies['portfolio'].id}` }} target="_blank" >
                     <button className="btn btn-primary m-1">Export</button>
                 </Link>
                 <Link to="/list">
