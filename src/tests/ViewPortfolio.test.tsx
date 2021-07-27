b1dc3e81fc8091a1c40d9e58d2b0d2cd7c44bbe5
Jake Geiser <geiser.jt@gmail.com>
2021-07-27 15:02:55 -0400

    added test file for viewPortfolio again

--- src/tests/ViewPortfolio.test.tsx
+++ src/tests/ViewPortfolio.test.tsx
@@ -3,10 +3,18 @@ import '@testing-library/jest-dom';
 
 import {url} from "../api/api";
 
-import {AdminUserTestComponent, GeneralUserTestComponent} from "../TestUserComponent";
+import {AdminUserTestComponent, GeneralUserTestComponent} from "./TestUserComponent";
 
-import IndustryEquivalencyView from '../../components/ViewPortfolio/IndustryEquivalencyView';
-import ViewPortfolio from "../../components/ViewPortfolio/ViewPortfolio";
+// Import Portfolio component viewers
+import AboutMeView from "../components/ViewPortfolio/AboutMeView";
+import CertificationView from "../components/ViewPortfolio/CertificationView";
+import EducationView from "../components/ViewPortfolio/EducationView";
+import HonorAwardView from "../components/ViewPortfolio/HonorAwardView";
+import IndustryEquivalencyView from '../components/ViewPortfolio/IndustryEquivalencyView';
+import OtherWorkExperienceView from "../components/ViewPortfolio/OtherWorkExperienceView";
+import ProjectView from "../components/ViewPortfolio/ProjectView";
+import RevatureWorkExperienceView from "../components/ViewPortfolio/RevatureWorkExperienceView";
+import ViewPortfolio from "../components/ViewPortfolio/ViewPortfolio";
 
 
 
@@ -15,7 +23,23 @@ beforeAll(async () => {
     <AdminUserTestComponent/>;
 })
 
+// describe test AboutMeView
 
+// describe test CertificationView
+
+// describe test EducationView
+
+// describe test HonorAwardView
+
+// describe test IndustryEquivalencyView
+
+// describe test OtherWorkExperienceView
+
+// describe test ProjectView
+
+// describe test RevatureWorkExperienceView
+
+// describe test ViewPortfolio
 describe('Test Admin functionality on viewing Portfolio', () => {
     
 
