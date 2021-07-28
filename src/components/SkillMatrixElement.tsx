6288bec49a9344917ca1cf61e2ccceb6ab8415ec
andregoulbourne <andre.goulbourne2@gmail.com>
2021-07-28 10:42:17 -0400

    Edited the skill Matrix Element

--- src/components/SkillMatrixElement.tsx
+++ src/components/SkillMatrixElement.tsx
@@ -1,36 +1,7 @@
-import { useState, FC, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { matrixUrl } from "../api/api";
-import { useCookies } from "react-cookie";
 import Matrix, { Skill } from "../interfaces/Matrix";
 import axios from "axios";
-import CanvasJSReact from "../canvasjs.react.js";
-
-var CanvasJS = CanvasJSReact.CanvasJS; 
-var CanvasJSChart = CanvasJSReact.CanvasJSChart;
-
-const options = {
-  exportEnabled: true,
-  animationEnabled: true,
-  title: {
-    text: "Website Traffic Sources"
-  },
-  data: [{
-    type: "pie",
-    startAngle: 75,
-    toolTipContent: "<b>{label}</b>: {y}%",
-    showInLegend: "true",
-    legendText: "{label}",
-    indexLabelFontSize: 16,
-    indexLabel: "{label} - {y}%",
-    dataPoints: [
-      { y: 18, label: "Direct" },
-      { y: 49, label: "Organic Search" },
-      { y: 9, label: "Paid Search" },
-      { y: 5, label: "Referral" },
-      { y: 19, label: "Social" }
-    ]
-  }]
-}
 
 export default function SkillMatrixElementUpdate(props : Matrix){
   const [element, updateElement] = useState(props);
@@ -65,8 +36,5 @@ export function SkillMatrixElementCreate(props : Matrix){
       axios.delete(`${matrixUrl}/${props.id}`)
         .then((response) => updateElement(response.data))
     }, []);
-  
-    return <>
-        matrices.
-    </>;
+
   };
