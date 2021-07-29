8d78a726d54672f6543fba3dfb1100ac8763059a
Nbot07 <nseobot@buffalo.edu>
2021-07-29 14:45:00 -0400

    fix duplication in Award and Certification validation tests

--- src/test/AwardValidation.test.tsx
+++ src/test/AwardValidation.test.tsx
@@ -1,97 +1,71 @@
-import { render } from "@testing-library/react";
 import awardValidation from "../components/validation/AwardValidation";
 
-test('ensure awardValidation returns false when passed falsy parameters', () => { 
-    let id = "";
-    let title = "";
-    let description = "";
-    let receivedFrom = "";
-    let dateReceived = "";
-    let portfolio = "";
+let id = "";
+let title = "";
+let description = "";
+let receivedFrom = "";
+let dateReceived = "";
+let portfolio = "";
 
+test('ensure awardValidation returns false when passed falsy parameters', () => { 
     let valid = awardValidation(id, title, description, receivedFrom, dateReceived, portfolio)
     expect(valid).toBe(false);
 });
 
 test('ensure awardValidation returns true when passed truthy parameters', () => { 
-    let id = "true";
-    let title = "true";
-    let description = "true";
-    let receivedFrom = "true";
-    let dateReceived = "true";
-    let portfolio = "true";
+    id = "true";
+    title = "true";
+    description = "true";
+    receivedFrom = "true";
+    dateReceived = "true";
+    portfolio = "true";
 
     let valid = awardValidation(id, title, description, receivedFrom, dateReceived, portfolio)
     expect(valid).toBe(true);
 });
 
 test('ensure awardValidation returns false when passed falsy id', () => { 
-    let id = "";
-    let title = "true";
-    let description = "true";
-    let receivedFrom = "true";
-    let dateReceived = "true";
-    let portfolio = "true";
+    id = "";
 
     let valid = awardValidation(id, title, description, receivedFrom, dateReceived, portfolio)
     expect(valid).toBe(false);
 });
 
 test('ensure awardValidation returns false when passed falsy title', () => { 
-    let id = "true";
-    let title = "";
-    let description = "true";
-    let receivedFrom = "true";
-    let dateReceived = "true";
-    let portfolio = "true";
+    id = "true";
+    title = "";
 
     let valid = awardValidation(id, title, description, receivedFrom, dateReceived, portfolio)
     expect(valid).toBe(false);
 });
 
 test('ensure awardValidation returns false when passed falsy description', () => { 
-    let id = "true";
-    let title = "true";
-    let description = "";
-    let receivedFrom = "true";
-    let dateReceived = "true";
-    let portfolio = "true";
+    title = "true";
+    description = "";
 
     let valid = awardValidation(id, title, description, receivedFrom, dateReceived, portfolio)
     expect(valid).toBe(false);
 });
 
 test('ensure awardValidation returns false when passed falsy recievedFrom', () => { 
-    let id = "true";
-    let title = "true";
-    let description = "true";
-    let receivedFrom = "";
-    let dateReceived = "true";
-    let portfolio = "true";
+    description = "true";
+    receivedFrom = "";
 
     let valid = awardValidation(id, title, description, receivedFrom, dateReceived, portfolio)
     expect(valid).toBe(false);
 });
 
 test('ensure awardValidation returns false when passed falsy dateReceived', () => { 
-    let id = "true";
-    let title = "true";
-    let description = "true";
-    let receivedFrom = "true";
-    let dateReceived = "";
-    let portfolio = "true";
+    receivedFrom = "true";
+    dateReceived = "";
 
     let valid = awardValidation(id, title, description, receivedFrom, dateReceived, portfolio)
     expect(valid).toBe(false);
 });
 
 test('ensure awardValidation returns false when passed falsy portfolio', () => { 
-    let id = "true";
-    let title = "true";
-    let description = "true";
-    let receivedFrom = "true";
-    let dateReceived = "true";
-    let portfolio = "";
+    dateReceived = "true";
+    portfolio = "";
 
     let valid = awardValidation(id, title, description, receivedFrom, dateReceived, portfolio)
     expect(valid).toBe(false);
