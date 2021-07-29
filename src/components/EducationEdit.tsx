54c3385dd74bddfc57382fe11fd1249b665255d1
molliemorrow <mfmorrow97@gmail.com>
2021-07-29 10:39:55 -0700

    Resolved Duplicates in Education components

---
+++ src/components/EducationEdit.tsx
@@ -0,0 +1,85 @@
+import axios from "axios";
+import React, { useState, FC, CSSProperties } from 'react'
+import { Button, Modal } from "react-bootstrap";
+import "../css/Project.css";
+import {url} from "../api/api";
+import educationValidation, { educationValidationErrors } from "./validation/EducationValidation";
+import styleInvalidElements, { styleInvalidElement } from "./validation/InvalidFormHandling";
+import ValidationMsg from './validation/ValidationMsg';
+import EducationData from "../interfaces/Education";
+import EducationUpdate from "./EducationUpdate";
+
+
+const EducationEdit: FC<{ hideModal: Function, university: string,
+     setUniversity: React.Dispatch<React.SetStateAction<string>>, degree: string, 
+     setDegree: React.Dispatch<React.SetStateAction<string>>, graduationDate: string,
+    setGraduationDate: React.Dispatch<React.SetStateAction<string>>, gpa: number,
+    setGpa: React.Dispatch<React.SetStateAction<number>>, logoUrl: string, 
+    setLogoUrl: React.Dispatch<React.SetStateAction<string>>}> = (props) => {
+    
+    return (
+        <div>
+            <Modal.Body>
+                <form method="post">
+                    <h6>University Name</h6>
+                    <input
+                        required
+                        type="text"
+                        name="university"
+                        className="form-input"
+                        value={props.university}
+                        onChange={(e) => props.setUniversity(e.target.value)}
+                    />
+                    <br />
+                    <h6>Degree Attained</h6>
+                    <input
+                        required
+                        type="text"
+                        name="degree"
+                        className="form-input"
+                        value={props.degree}
+                        onChange={(e) =>
+                            props.setDegree(e.target.value)
+                        }
+                    />
+                    <br />
+                    <h6>Graduation Date</h6>
+                    <input
+                        required
+                        type="date"
+                        name="graduationDate"
+                        className="form-input"
+                        value={props.graduationDate}
+                        onChange={(e) =>
+                            props.setGraduationDate(e.target.value)
+                        }
+                    />
+                    <br />
+                    <h6>GPA</h6>
+                    <input
+                        required
+                        type="number"
+                        name="gpa"
+                        className="form-input"
+                        value={props.gpa}
+                        onChange={(e) => props.setGpa(Number(e.target.value))}
+                    />
+                    <br />
+                    <h6 className="logoUrl">URL for University Logo (Optional)</h6>
+                    <input
+                        type="text"
+                        name="logoUrl"
+                        className="form-input-optional"
+                        value={props.logoUrl}
+                        onChange={(e) => props.setLogoUrl(e.target.value)}
+                    />
+                </form>
+
+                {/* <ValidationMsg errors={validationErrors}></ValidationMsg> */}
+
+            </Modal.Body>
+        </div>
+    );
+};
+
+export default EducationEdit;
