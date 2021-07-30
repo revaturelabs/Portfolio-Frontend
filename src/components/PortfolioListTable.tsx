648274113c9aacc0e31eef13745721a3700e3f21
ramarier11 <48192184+ramarier11@users.noreply.github.com>
2021-07-30 12:20:07 -0400

    Added sort functionality and delete ability for user table

---
+++ src/components/PortfolioListTable.tsx
@@ -0,0 +1,159 @@
+import axios from "axios";
+import React, { useMemo, useState } from "react";
+import { Table, Button } from "react-bootstrap";
+import { useCookies } from "react-cookie";
+import { toast } from "react-toastify";
+import { portfolioUrl } from "../api/api";
+
+function PortfolioListTable(props: any) {
+  let { portfolios } = props;
+  const [, setCookie, removeCookie] = useCookies();
+  const [sortConfig, setSortConfig]: any = useState("approved");
+  let sortedPortfolios = [...portfolios];
+  console.log(portfolios);
+
+  const defaultArrows = {
+    id: "—",
+    name: "—",
+    submitted: "—",
+    approved: "—",
+    reviewed: "—",
+  };
+  const [directionArrows, setDirections] = useState(defaultArrows);
+
+  useMemo(() => {
+    if (sortConfig !== null) {
+      sortedPortfolios.sort((a, b) => {
+        if (a[sortConfig.key] < b[sortConfig.key]) {
+          return sortConfig.direction === "ascending" ? -1 : 1;
+        }
+        if (a[sortConfig.key] > b[sortConfig.key]) {
+          return sortConfig.direction === "ascending" ? 1 : -1;
+        }
+        return 0;
+      });
+    }
+    return sortedPortfolios;
+  }, [sortedPortfolios, sortConfig]);
+
+  const requestSort = (key: any) => {
+    let direction = "ascending";
+    if (sortConfig.key === key && sortConfig.direction === "ascending") {
+      direction = "descending";
+    }
+    if (key === "id" && direction === "descending") {
+      setDirections({ ...defaultArrows, id: "⯆" });
+    }
+    if (key === "id" && direction === "ascending") {
+      setDirections({ ...defaultArrows, id: "⯅" });
+    }
+    if (key === "name" && direction === "descending") {
+      setDirections({ ...defaultArrows, name: "⯆" });
+    }
+    if (key === "name" && direction === "ascending") {
+      setDirections({ ...defaultArrows, name: "⯅" });
+    }
+    if (key === "submitted" && direction === "descending") {
+      setDirections({ ...defaultArrows, submitted: "⯆" });
+    }
+    if (key === "submitted" && direction === "ascending") {
+      setDirections({ ...defaultArrows, submitted: "⯅" });
+    }
+    if (key === "approved" && direction === "descending") {
+      setDirections({ ...defaultArrows, approved: "⯆" });
+    }
+    if (key === "approved" && direction === "ascending") {
+      setDirections({ ...defaultArrows, approved: "⯅" });
+    }
+    if (key === "reviewed" && direction === "descending") {
+      setDirections({ ...defaultArrows, reviewed: "⯆" });
+    }
+    if (key === "reviewed" && direction === "ascending") {
+      setDirections({ ...defaultArrows, reviewed: "⯅" });
+    }
+    setSortConfig({ key, direction });
+  };
+
+  const handlePortfolioEdit = (id: any, submitted: boolean) => {
+    let pathname = submitted ? "./view" : "./portfolio";
+    axios
+      .get(`${portfolioUrl}/${id}`)
+      .then((response) => {
+        setCookie("portfolio", response.data, { path: "/" });
+        window.location.pathname = pathname;
+      })
+      .catch((error) => {
+        toast.error(error.message);
+      });
+  };
+
+  const handleDelete = (id: any) => {
+    axios
+      .delete(`${portfolioUrl}/${id}`)
+      .then((response) => {
+        removeCookie("portfolio", { maxAge: 0 });
+        toast.success("Portfolio deleted");
+      })
+      .catch((error) => {
+        toast.error(error.message);
+      });
+  };
+
+  return (
+    <Table style={{ margin: "10px" }} striped table-bordered hover>
+      <thead>
+        <tr>
+          <th onClick={() => requestSort("id")}># {directionArrows.id}</th>
+          <th onClick={() => requestSort("name")}>
+            Portfolio Name {directionArrows.name}{" "}
+          </th>
+          <th onClick={() => requestSort("submitted")}>
+            Submitted {directionArrows.submitted}
+          </th>
+          <th onClick={() => requestSort("reviewed")}>
+            Reviewed {directionArrows.reviewed}
+          </th>
+          <th onClick={() => requestSort("approved")}>
+            Approved {directionArrows.approved}
+          </th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {sortedPortfolios.map((portfolio: any) => {
+          return (
+            <tr key={portfolio.id} id='table-rows'>
+              <td>{portfolio.id}</td>
+              <td>{portfolio.name}</td>
+              <td>{portfolio.submitted ? "Submitted" : "Pending"}</td>
+              <td>
+                {portfolio.reviewed ? "Review Completed" : "Not Reviewed"}
+              </td>
+              <td>{portfolio.approved ? "Approved" : "Rejected"}</td>
+
+              <td>
+                <Button
+                  variant='danger'
+                  style={{ marginRight: "10px" }}
+                  onClick={() => handleDelete(portfolio.id)}
+                >
+                  Delete
+                </Button>
+                <Button
+                  variant='primary'
+                  onClick={() =>
+                    handlePortfolioEdit(portfolio.id, portfolio.submitted)
+                  }
+                >
+                  {portfolio.submitted ? "View" : "Edit"}
+                </Button>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </Table>
+  );
+}
+
+export default PortfolioListTable;
