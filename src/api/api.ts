a245fa829cf225baac7bd8cd22b9ed30fafde394
andregoulbourne <andre.goulbourne2@gmail.com>
2021-08-01 14:22:38 -0400

    resolving merge conflict

---
+++ src/api/api.ts
@@ -0,0 +1,11 @@
+export const url = 'http://localhost:8081/api'
+
+export const aboutMeUrl = url + "/aboutMe"
+export const portfolioUrl = url + "/portfolios"
+export const educationUrl = url + "/education"
+export const honorUrl = url + "/honor"
+export const equivUrl = url + "/equiv"
+export const workHistoryUrl = url + "/workhistory"
+export const projectUrl = url + "/projects"
+export const workExperienceUrl = url + "/workexperience"
+export const matrixUrl = url + "/matrix"
